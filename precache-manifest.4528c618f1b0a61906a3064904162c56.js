4c11832bf74649266c69ac8d6b99ff1dda878742
Annie <prashantyogananda@gmail.com>
2022-04-19 01:16:47 +0530

    Updates

---
+++ precache-manifest.4528c618f1b0a61906a3064904162c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d4aedac5df1b94ff68c8caa862c8af",
+    "url": "/elc-coding-test/index.html"
+  },
+  {
+    "revision": "816d26ab2eff7254d37a",
+    "url": "/elc-coding-test/static/css/main.df260a95.chunk.css"
+  },
+  {
+    "revision": "d5886122f6c411bde47e",
+    "url": "/elc-coding-test/static/js/2.bb21e0d3.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/elc-coding-test/static/js/2.bb21e0d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816d26ab2eff7254d37a",
+    "url": "/elc-coding-test/static/js/main.eac74bf6.chunk.js"
+  },
+  {
+    "revision": "8ca072bf9b07a93deadc",
+    "url": "/elc-coding-test/static/js/runtime-main.e8f38fcb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/elc-coding-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/elc-coding-test/precache-manifest.4528c618f1b0a61906a3064904162c56.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/elc-coding-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
